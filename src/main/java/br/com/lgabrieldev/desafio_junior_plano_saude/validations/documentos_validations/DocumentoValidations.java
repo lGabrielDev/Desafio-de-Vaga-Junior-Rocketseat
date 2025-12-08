635463fd533058ae2dd8e6d5027be296e13a3b43
lGabrielDev <thegabrielfreitasbf@yahoo.com.br>
2025-12-08 20:37:43 -0300

    documentoValidations convertido todos os methods para lambda

--- src/main/java/br/com/lgabrieldev/desafio_junior_plano_saude/validations/documentos_validations/DocumentoValidations.java
+++ src/main/java/br/com/lgabrieldev/desafio_junior_plano_saude/validations/documentos_validations/DocumentoValidations.java
@@ -1,5 +1,4 @@
 package br.com.lgabrieldev.desafio_junior_plano_saude.validations.documentos_validations;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,12 +18,11 @@ public  class DocumentoValidations implements DocumentoValidationImp{
      //attributes
      private final static Set<String> TIPOS_DOCUMENTOS_VALIDOS = Set.of("RG", "CPF", "CNH");
 
-     // ****************** validacoes do campo 'tipoDocumento' ****************** 
+     // ****************************************************** validacoes do campo 'tipoDocumento' ****************************************************** 
      @Override
      public  Boolean tipoDocumentoNaoPodeSerNull(List<DocumentoCreateDto> documentos) {
 
                Boolean tipoDocumentoErrado =  documentos.stream().anyMatch( i -> i.getTipoDocumento() == null || i.getTipoDocumento().isBlank());
-               
                if(tipoDocumentoErrado){
                     throw new CampoNaoPodeSerNullException("O campo 'tipoDocumento' não pode ser null.");
                }
@@ -36,14 +34,12 @@ public  class DocumentoValidations implements DocumentoValidationImp{
 
           Boolean tipoInvalidoEncontrado = documentos.stream().anyMatch((document) -> {
                String tipoDocumento = document.getTipoDocumento();
-
                return !(TIPOS_DOCUMENTOS_VALIDOS.contains(tipoDocumento.toUpperCase()));
           });
-
-               if(tipoInvalidoEncontrado){
-                    throw new TipoDocumentoNaoEncontradoException(" 'tipoDocumento' não encontrado!  Os tipos disponíveis são:\n'RG', 'CPF' e 'CNH' ");
-               }
-               return true;
+          if(tipoInvalidoEncontrado){
+               throw new TipoDocumentoNaoEncontradoException(" 'tipoDocumento' não encontrado!  Os tipos disponíveis são:\n'RG', 'CPF' e 'CNH' ");
+          }
+          return true;
      }
 
      @Override
@@ -51,7 +47,6 @@ public  class DocumentoValidations implements DocumentoValidationImp{
 
          Map<String, Long>  tipoDocumentoAgrupado = documentos.stream()
                .collect(Collectors.groupingBy(
-
                     //lembrando, que aqui o tipo de documento já está correto porque verificamos no method acima ^^
                     document -> document.getTipoDocumento().toUpperCase(), //  1º parametro --> como quero agrupar
                     Collectors.counting() //  2º parametro --> a informacao que quero informar. Se fosse campo de valor, poderia somar, etc...
@@ -61,16 +56,13 @@ public  class DocumentoValidations implements DocumentoValidationImp{
                     System.out.println(String.format("%s --> %d", chave, valor));
              });
 
-
           Boolean temDocumentoDuplicado = tipoDocumentoAgrupado.values().stream()
                .anyMatch((valor) ->  valor >= 2);
-
                if(temDocumentoDuplicado){
                     throw new BeneficiarioApenasPodeTer1DocumentoPorTipoException("Nao eh possível ter mais de 1 CPF\nNão eh possivel ter mais de 1 RG\nNao eh possível ter mais de 1 CNH");
                };
           return true;
      }
-
 
      @Override
      public Boolean tipoDocumentoTudoCerto(List<DocumentoCreateDto> documentos) {
@@ -80,72 +72,65 @@ public  class DocumentoValidations implements DocumentoValidationImp{
           return true;
      }
 
-     // ****************** validacoes do campo 'numeroDocumento' ******************
-
-
-
-
-     // CONTINUAR AQUI -->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+     // ****************************************************** validacoes do campo 'numeroDocumento' ******************************************************
      @Override
      public Boolean numeroDocumentoNaoPodeSerNull(List<DocumentoCreateDto> documentos) {
-           for(DocumentoCreateDto i : documentos){
-                if(i.getNumeroDocumento() == null || i.getNumeroDocumento().isBlank()){
-                    throw new CampoNaoPodeSerNullException("O campo 'numeroDocumento' não pode ser null.");
-               }
-           }
-          return true;
-     }
 
-
-     @Override
-     public Boolean numeroDocumentoQuantidadeCharacteresEstaCorreto(List<DocumentoCreateDto> documentos) {
-          String tipoDocumento = "";
-          String numeroDocumento = "";
-
-          for(DocumentoCreateDto i : documentos){
-                    tipoDocumento = i.getTipoDocumento();
-                    numeroDocumento = i.getNumeroDocumento();
-
-               switch (tipoDocumento.toUpperCase()) {
-                    case "CPF":
-                         if(numeroDocumento.length() != 11){
-                              throw new CampoTemMuitosCharactersException(" 'CPF' deve ter exatamente 11 dígitos");
-                         }
-                         break;
-                    case "RG":
-                         if(numeroDocumento.length() != 9 ){
-                              throw new CampoTemMuitosCharactersException("  'RG' deve ter exatamente 9 dígitos");   
-                         }
-                         break;
-                    case "CNH":
-                         if(numeroDocumento.length() != 11){
-                              throw new CampoTemMuitosCharactersException(" 'CNH' deve ter exatamente 11 dígitos");
-                         }
-                         break;
-               }
+          Boolean numeroDocumentoErrado =  documentos.stream().anyMatch( i -> i.getNumeroDocumento() == null || i.getNumeroDocumento().isBlank());
+          if(numeroDocumentoErrado){
+               throw new CampoNaoPodeSerNullException("O campo 'numeroDocumento' não pode ser null.");
           }
           return true;
      }
+     
+     @Override
+     public Boolean numeroDocumentoQuantidadeCharacteresEstaCorreto(List<DocumentoCreateDto> documentos) {
 
+        documentos.stream().anyMatch((documento) -> {
+               String tipoDocumento = documento.getTipoDocumento();
+               String numeroDocumento = documento.getNumeroDocumento();
+               String errorMessage = "";
+               Boolean quantidadeErrada = false;
+
+               switch (tipoDocumento.toUpperCase()) {
+                    case "RG":
+                         if(numeroDocumento.length() != 9){
+                              quantidadeErrada = true;
+                              errorMessage = String.format(" '%s' deve ter exatamente 9 characteres! ", tipoDocumento);
+                         }
+                         break;
+                    case "CPF":
+                    case "CNH":
+                         if(numeroDocumento.length() != 11){
+                              quantidadeErrada = true;
+                              errorMessage = String.format(" '%s' deve ter exatamente 11 characteres! ", tipoDocumento);
+                         }
+                         break;
+               }    
+                 if(quantidadeErrada){
+                         throw new CampoTemMuitosCharactersException(errorMessage);
+                    }
+               return false;       // no anyMatch, ou qualquer outro method de filtro, o loop só vai parar se ele retornar true. Enquanto retornar false, o proximo loop acontece.
+          }); 
+          return true;
+     }
 
      @Override
      public Boolean numeroDocumentoApenasNumeros(List<DocumentoCreateDto> documentos) {
-          String numeroDocumento = "";
-
-          for(DocumentoCreateDto i : documentos){
-               numeroDocumento = i.getNumeroDocumento();
+          documentos.stream().anyMatch(documento -> {
+               String numeroDocumento = documento.getNumeroDocumento();
                if(  !(numeroDocumento.chars().allMatch(c ->  Character.isDigit(c))) ) {
                     throw new CampoDeveTerApenasNumerosException(" 'numeroDocumento' deve possuir apenas números.");
                }
-          }
-          return true;
+               return false;  // no anyMatch, ou qualquer outro method de filtro, o loop só vai parar se ele retornar true. Enquanto retornar false, o proximo loop acontece.
+          });
+          return true;     
      }
 
      @Override
      public Boolean numeroDocumentoTudoCerto(List<DocumentoCreateDto> documentos) {
      
           this.numeroDocumentoNaoPodeSerNull(documentos);
-          
           this.numeroDocumentoQuantidadeCharacteresEstaCorreto(documentos);
           this.numeroDocumentoApenasNumeros(documentos);
           return true;
@@ -158,7 +143,6 @@ public  class DocumentoValidations implements DocumentoValidationImp{
 
           this.tipoDocumentoTudoCerto(documentos);
           this.numeroDocumentoTudoCerto(documentos);
-
           return true;
      }
 }
