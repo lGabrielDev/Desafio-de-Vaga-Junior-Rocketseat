2cb2333c0cc795f460581618b1b4367c16e47d1d
lGabrielDev <thegabrielfreitasbf@yahoo.com.br>
2025-12-13 14:14:40 -0300

    alterado a ordem da listagem dos beneficiarios

--- src/main/java/br/com/lgabrieldev/desafio_junior_plano_saude/models/beneficiario/repository/BeneficiarioRepository.java
+++ src/main/java/br/com/lgabrieldev/desafio_junior_plano_saude/models/beneficiario/repository/BeneficiarioRepository.java
@@ -1,15 +1,13 @@
 package br.com.lgabrieldev.desafio_junior_plano_saude.models.beneficiario.repository;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import br.com.lgabrieldev.desafio_junior_plano_saude.models.beneficiario.Beneficiario;
 
-
 public interface BeneficiarioRepository extends JpaRepository<Beneficiario, Long> {
      
-     @Query(value = "SELECT b FROM Beneficiario b ORDER BY b.id ASC")
+     @Query(value = "SELECT b FROM Beneficiario b ORDER BY b.id DESC")
      public List<Beneficiario> findAll();
 
 
